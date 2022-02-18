373b529fb8bd1e265f2e925ed066064d2c3cf2dc
Brawer Nuñez <n.eedar@hotmail.com>
2022-02-18 18:35:00 -0500

    add android home environment variable for fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,12 @@ alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
 # Prompt
 
 starship init fish | source
+
+# Android Home environment variable
+
+set --export ANDROID_HOME $HOME/Android/Sdk
+set -gx PATH $ANDROID_HOME/emulator $PATH;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+
